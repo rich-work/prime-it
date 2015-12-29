73b6d48a8e9fe47c985031b97f4f4462741e783a
IT Mac <itmac@its-imac.local>
2015-12-29 08:46:17 -0600

    Initial Commit

---
+++ Prime-ITTests/Prime_ITTests.swift
@@ -0,0 +1,36 @@
+//
+//  Prime_ITTests.swift
+//  Prime-ITTests
+//
+//  Created by IT Mac on 12/29/15.
+//  Copyright © 2015 IT Mac. All rights reserved.
+//
+
+import XCTest
+@testable import Prime_IT
+
+class Prime_ITTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
